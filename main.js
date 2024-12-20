5c4a9db57b3bfaaf427bda150c7da47737b05388
Khxnsu <thekhonsu@proton.me>
2024-12-20 11:58:35 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+require.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs' }});
+
+window.MonacoEnvironment = {
+    getWorkerUrl: function(workerId, label) {
+        return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+            self.MonacoEnvironment = {
+                baseUrl: '${location.origin}/node_modules/monaco-editor/min/'
+            };
+            importScripts('${location.origin}/node_modules/monaco-editor/min/vs/base/worker/workerMain.js');`
+        )}`;
+    }
+};
+
+require(['vs/editor/editor.main'], function() {
+    monaco.editor.create(document.getElementById('container'), {
+        value: '',
+        language: 'lua',
+        theme: 'vs-dark'
+    });
+});
